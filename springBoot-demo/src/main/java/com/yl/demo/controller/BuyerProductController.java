854f356bd2cb0750fcf6bba6067a576ddf250697
yelu082359 <1940927916@qqcom>
2018-06-24 18:55:02 +0800

    卖家商品功能提交

--- springBoot-demo/src/main/java/com/yl/demo/controller/BuyerProductController.java
+++ springBoot-demo/src/main/java/com/yl/demo/controller/BuyerProductController.java
@@ -1,12 +1,7 @@
 package com.yl.demo.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,58 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yl.demo.VO.ProductInfoVO;
 import com.yl.demo.VO.ProductVO;
 import com.yl.demo.VO.ResultVO;
-import com.yl.demo.dataobject.ProductCategory;
-import com.yl.demo.dataobject.ProductInfo;
-import com.yl.demo.service.CategoryService;
-import com.yl.demo.service.ProductService;
-import com.yl.demo.utils.ResultVOUtil;
 
 @RestController
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
-
-	@Autowired
-	private ProductService productService;
-
-	@Autowired
-	private CategoryService categoryService;
-
+	
 	@GetMapping("/list")
-	public ResultVO list() {
-
-		// 1.搜索所有上架商品
-		List<ProductInfo> productInfos = productService.findUpAll();
-		// 2.查讯类目（一次性查询）
-		// 传统方法
-		/*
-		 * List<Integer> categoryTypeList = new ArrayList<Integer>(); for(ProductInfo
-		 * productInfo : productInfos) {
-		 * categoryTypeList.add(productInfo.getCategoryType()); }
-		 */
-		// 精简方法（Java8，lambda）
-		List<Integer> categoryTypeList = productInfos.stream().map(e -> e.getCategoryType())
-				.collect(Collectors.toList());
-		List<ProductCategory> productCategories = categoryService.findByCategoryTypeIn(categoryTypeList);
-
-		// 3.数据拼装
-		List<ProductVO> productVOs = new ArrayList<ProductVO>();
-		for (ProductCategory productCategory : productCategories) {
-			ProductVO productVO = new ProductVO();
-			productVO.setCategoryName(productCategory.getCategoryName());
-			productVO.setCategoryType(productCategory.getCategoryType());
-
-			List<ProductInfoVO> productInfoVOs = new ArrayList<ProductInfoVO>();
-			for (ProductInfo productInfo : productInfos) {
-				if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
-					ProductInfoVO productInfoVO = new ProductInfoVO();
-					BeanUtils.copyProperties(productInfo, productInfoVO);
-					productInfoVOs.add(productInfoVO);
-				}
-			}
-			productVO.setProductInfoVOList(productInfoVOs);
-			productVOs.add(productVO);
-		}
+	public ResultVO list(){
+		ResultVO resultVo = new ResultVO();
+		ProductVO productVo = new ProductVO();
+		ProductInfoVO productInfoVO = new ProductInfoVO();
 		
-		return ResultVOUtil.success(productVOs);
+		productVo.setProductInfoVOList(Arrays.asList(productInfoVO));
+		resultVo.setData(Arrays.asList(productVo));
+		resultVo.setCode(0);
+		resultVo.setMsg("成功");
+		return resultVo;
 	}
 }
